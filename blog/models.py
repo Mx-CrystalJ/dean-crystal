3a3f182aa6fbb54e32780d451ec88345ebe41a3c
Crystal Johnson <mxcrystaljohnson@gmail.com>
2024-12-13 18:41:33 +0000

    Add class meta & model method for blog posts.

--- blog/models.py
+++ blog/models.py
@@ -18,6 +18,12 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["-updated_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author_id}"
+
 
 class Comment(models.Model):
     comment_id = models.AutoField(primary_key=True)
