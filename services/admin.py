3c0f6e0ba9a419bf0186cda6b30500c378adccd6
Crystal Johnson <mxcrystaljohnson@gmail.com>
2024-12-31 12:03:18 +0000

    REname price to min price and add max price to service model, better admin controls for services based on testing.

--- services/admin.py
+++ services/admin.py
@@ -5,7 +5,7 @@ from .models import Service, Order
 # Register your models here.
 @admin.register(Service)
 class ServiceAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'category', 'word_count_range', 'turnaround_time',)
+    list_display = ('name', 'min_price', 'max_price', 'category', 'word_count_range', 'turnaround_time',)
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
